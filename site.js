5a9d93b66611b0bc60c73a5a6f9a67a05c39edc6
McIntosh, Kaegan E <800063157@fvtc.edu>
2026-01-21 14:58:27 -0600

    Added Welcome Message

--- site.js
+++ site.js
@@ -1,1 +1,18 @@
 console.log("Java Script Connected");
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const Time = document.querySelector('#welcome');
+if (isMorning) {
+    Time.textContent = ' Welcome and Good Morning,';
+}
+else if (isAfternoon) {
+    Time.textContent = 'Welcome and Good Afternoon,';
+}
+else if (isEvening) {
+    Time.textContent = 'Welcome and Good Evening,';
+}
